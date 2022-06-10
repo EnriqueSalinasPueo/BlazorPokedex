3c5c4558ce3c30062d13b089f57a13c32a0b412a
Enrique Salinas <kikotes8686@gmail.com>
2022-06-10 14:15:06 +0200

    Comentario

--- BlazorPokedex.Models/Sprite.cs
+++ BlazorPokedex.Models/Sprite.cs
@@ -4,6 +4,8 @@ namespace BlazorPokedex.Models
 {
     public class Sprite
     {
+        // Las etiquetas se ponen por que el nombre que hemos dado en c#
+        // no coincide con el nombre de la etiqueta del json y sino no se puede deserializar
         [JsonProperty("front_default")]
         public string Front { get; set; }
         [JsonProperty("back_default")]
